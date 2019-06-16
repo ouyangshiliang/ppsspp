7623bc6fae11c5c843480a55bd89021f3bde7244
Henrik Rydgård <hrydgard@gmail.com>
2019-06-16 21:57:22 +0200

    Vulkan: Add a single background thread for pipeline creation

--- ext/native/thin3d/VulkanQueueRunner.cpp
+++ ext/native/thin3d/VulkanQueueRunner.cpp
@@ -1,4 +1,6 @@
 #include <map>
+
+#include "base/timeutil.h"
 #include "DataFormat.h"
 #include "VulkanQueueRunner.h"
 #include "VulkanRenderManager.h"
@@ -1014,9 +1016,10 @@ void VulkanQueueRunner::PerformRenderPass(const VKRStep &step, VkCommandBuffer c
 		{
 			VKRGraphicsPipeline *pipeline = c.graphics_pipeline.pipeline;
 			if (!pipeline->pipeline) {
-				// Late! Compile it.
-				if (!pipeline->Create(vulkan_))
-					break;
+				// Stall processing, waiting for the compile queue to catch up.
+				// TODO: Switch to a cond_var here too.
+				while (!pipeline->pipeline)
+					sleep_ms(1);
 			}
 			if (pipeline->pipeline != lastGraphicsPipeline) {
 				vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline->pipeline);
@@ -1033,9 +1036,10 @@ void VulkanQueueRunner::PerformRenderPass(const VKRStep &step, VkCommandBuffer c
 		{
 			VKRComputePipeline *pipeline = c.compute_pipeline.pipeline;
 			if (!pipeline->pipeline) {
-				// Late! Compile it.
-				if (!pipeline->Create(vulkan_))
-					break;
+				// Stall processing, waiting for the compile queue to catch up.
+				// TODO: Switch to a cond_var here too.
+				while (!pipeline->pipeline)
+					sleep_ms(1);
 			}
 			if (pipeline->pipeline != lastComputePipeline) {
 				vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline->pipeline);
